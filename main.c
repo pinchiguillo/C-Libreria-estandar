0b24526f19b18ff1418b237d837920a9070f2536
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-11-20 11:20:13 +0100

    Resolucion Ejercicio

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+// Created by pinchiguillo on 20/11/2024.
+//
+
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    char str1[] = "Hola";
+    char str2[] = "Mundo";
+
+    int comparison = strcmp(str1, str2);
+
+    if (comparison == 0) {
+        printf("Las cadenas son iguales.\n");
+    } else {
+        printf("Las cadenas son diferentes.\n");
+    }
+
+    return 0;
+}
